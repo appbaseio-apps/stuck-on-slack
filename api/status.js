26ca6bbc7576fc96552e8dfbc757341ee422eb8a
Yash Shah <mail@yashshah.com>
2016-05-21 21:55:54 +0530

    Update status.js

--- api/status.js
+++ api/status.js
@@ -3,8 +3,11 @@ var appbaseRef = require('../helper/appbase').appbaseRef;
 module.exports = function(req, res) {
   res.json({ text: 'Your status has been updated! Keep working!' });
   var jsonObject = {
+    "team_id": req.query.team_id,
+    "team_domain": req.query.team_domain,
     "status": req.query.text,
-    "user_name": req.query.user_name
+    "user_name": req.query.user_name,
+    "channel_name": req.query.channel_name
   }
   console.log(jsonObject)
   appbaseRef.index({
